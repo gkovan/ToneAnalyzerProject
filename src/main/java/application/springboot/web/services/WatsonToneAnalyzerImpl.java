3ed471040f2376e0cff87fc7cc197d6f36af3651
gkovan <gkovan@hotmail.com>
2018-03-09 08:22:44 -0600

    minor update.

--- src/main/java/application/springboot/web/services/WatsonToneAnalyzerImpl.java
+++ src/main/java/application/springboot/web/services/WatsonToneAnalyzerImpl.java
@@ -26,19 +26,19 @@ public class WatsonToneAnalyzerImpl implements WatsonToneAnalyzer {
 		ToneAnalyzer service = new ToneAnalyzer(VERSION_DATE);
 		service.setUsernameAndPassword("43233b68-ae7e-4804-b7b5-3302597d1834", "g8a4Lt6g5jrT");
 
-		String text = "I know the times are difficult! Our sales have been "
-				+ "disappointing for the past three quarters for our data analytics "
-				+ "product suite. We have a competitive data analytics product "
-				+ "suite in the industry. But we need to do our job selling it! "
-				+ "We need to acknowledge and fix our sales challenges. "
-				+ "We can’t blame the economy for our lack of execution! "
-				+ "We are missing critical sales opportunities. "
-				+ "Our product is in no way inferior to the competitor products. "
-				+ "Our clients are hungry for analytical tools to improve their "
-				+ "business outcomes. Economy has nothing to do with it.";
+//		String text = "I know the times are difficult! Our sales have been "
+//				+ "disappointing for the past three quarters for our data analytics "
+//				+ "product suite. We have a competitive data analytics product "
+//				+ "suite in the industry. But we need to do our job selling it! "
+//				+ "We need to acknowledge and fix our sales challenges. "
+//				+ "We can’t blame the economy for our lack of execution! "
+//				+ "We are missing critical sales opportunities. "
+//				+ "Our product is in no way inferior to the competitor products. "
+//				+ "Our clients are hungry for analytical tools to improve their "
+//				+ "business outcomes. Economy has nothing to do with it.";
 
 		// Call the service and get the tone
-		ToneOptions toneOptions = new ToneOptions.Builder().html(text).build();
+		ToneOptions toneOptions = new ToneOptions.Builder().html(sentence).build();
 		ToneAnalysis tone = service.tone(toneOptions).execute();
 		System.out.println(tone);
 
